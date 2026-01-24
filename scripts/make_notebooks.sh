f24ce61a2151bd94be6d9052198f0624b5e2ccea
cpp-tutor <cpptutor@outlook.com>
2026-01-24 15:22:33 +0000

    Allow spaces in paths

--- scripts/make_notebooks.sh
+++ scripts/make_notebooks.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-srcdir=$(dirname $(readlink -f "$0"))/..
-destdir=$srcdir/jupyter-notebooks
-for f in $srcdir/??-*.md ; do
-  out=$destdir/$(basename $f)
-  rm -f ${out%%.md}.ipynb
-  cat $f \
+srcdir="$(dirname "$(readlink -f "$0")")/.."
+destdir="$srcdir/jupyter-notebooks"
+for f in "$srcdir"/??-*.md ; do
+  [ -e "$f" ] || continue
+  out="$destdir/$(basename "$f")"
+  rm -f "${out%%.md}.ipynb"
+  cat "$f" \
     | sed '/```bash/{N; s/```bash\n/```python\n!/}' \
     | sed '/```cpp/{N; s/```cpp\n\/\/ /```python\n%%writefile /}' \
     | sed '/```cmake/{N; s/```cmake\n/```python\n%%writefile CMakeLists.txt\n/}' \
-    | jupytext --from md --to notebook -k python3 -o ${out%%.md}.ipynb
+    | jupytext --from md --to notebook -k python3 -o "${out%%.md}.ipynb"
 done
